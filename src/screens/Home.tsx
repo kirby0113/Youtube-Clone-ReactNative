f035083dd73014f26df5f1f961f4143978ba4503
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-28 23:00:04 +0900

    feat: Cardコンポーネントを作成
    feat: Card.tsxを作成
    feat: HomeにCardを表示

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import Header from '../components/Header';
+import Card from '../components/Card';
 
 const Home = () => {
   return (
     <View>
       <Header />
-      <Text>home screen</Text>
+      <Card />
+      <Card />
     </View>
   );
 };
