ed296ab80ac49fdddf1fe5b2159abac7d12b7005
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-29 20:11:13 +0900

    feat: VideoPlayerページを作成

---
+++ src/screens/VideoPlayer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const VideoPlayer = () => {
+  return (
+    <View style={{flex: 1, marginTop: 40}}>
+      <Text>VideoPlayer</Text>
+    </View>
+  );
+};
+
+export default VideoPlayer;
