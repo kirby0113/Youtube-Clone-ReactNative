ee2f8d920054747888ca3df1d7fdb89799e87226
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-29 20:10:44 +0900

    feat: Suscribeページを作成

---
+++ src/screens/Suscribe.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+import Header from '../components/Header';
+
+const Suscribe = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Header />
+      <Text>Suscribe</Text>
+    </View>
+  );
+};
+
+export default Suscribe;
