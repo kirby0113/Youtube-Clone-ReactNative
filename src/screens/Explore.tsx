39266b5f638bf2190a4430a3a9445eb9f2f07ddd
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-29 22:26:16 +0900

    feat: Exploreページにデータ表示

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,12 +1,71 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, ScrollView} from 'react-native';
 import Header from '../components/Header';
+import Card from '../components/Card';
+
+import {useSelector} from 'react-redux';
+
+const LittleCard = ({name}) => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'red',
+        height: 50,
+        width: 180,
+        borderRadius: 4,
+        marginTop: 10,
+      }}>
+      <Text
+        style={{
+          textAlign: 'center',
+          color: 'white',
+          fontSize: 22,
+          marginTop: 5,
+        }}>
+        {name}
+      </Text>
+    </View>
+  );
+};
 const Explore = () => {
+  const cardData = useSelector(state => {
+    return state;
+  });
   return (
     <View style={{flex: 1}}>
       <Header />
-      <Text>Explore</Text>
+      <ScrollView nestedScrollEnabled={true}>
+        <View
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+          }}>
+          <LittleCard name="Gaming" />
+          <LittleCard name="Trending" />
+          <LittleCard name="Music" />
+          <LittleCard name="News" />
+          <LittleCard name="Movies" />
+          <LittleCard name="Fashion" />
+        </View>
+        <Text style={{margin: 8, fontSize: 22, borderBottomWidth: 1}}>
+          Trending Videos
+        </Text>
+        <FlatList
+          data={cardData}
+          renderItem={({item}) => {
+            return (
+              <Card
+                videoId={item.id.videoId}
+                title={item.snippet.title}
+                channelTitle={item.snippet.channelTitle}
+              />
+            );
+          }}
+          keyExtractor={item => item.id.videoId}
+        />
+      </ScrollView>
     </View>
   );
 };
