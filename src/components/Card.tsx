242c049e2e86edd291e3ac0deb2ee2dc9d583b42
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-12-30 12:24:13 +0900

    fix: Cardの修正
    fix: screennameの修正
    fix: CardからVideoPlayerにアクセスできるように修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 import {default as MaterialIcons} from 'react-native-vector-icons/MaterialIcons';
 
 const Card = props => {
+  const navigation = useNavigation();
   return (
-    <View style={style.Card}>
-      <Image
-        source={{
-          uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
-        }}
-        style={{width: '100%', height: 200}}
-      />
-      <View style={style.CardDetail}>
-        <MaterialIcons name="account-circle" size={40} color="#212121" />
-        <View style={style.CardTexts}>
-          <Text style={style.CardTitle} ellipsizeMode="tail" numberOfLines={2}>
-            {props.title}
-          </Text>
-          <Text>{props.channelTitle}</Text>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('videoplayer', {
+          videoId: props.videoId,
+          title: props.title,
+        })
+      }>
+      <View style={style.Card}>
+        <Image
+          source={{
+            uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
+          }}
+          style={{width: '100%', height: 200}}
+        />
+        <View style={style.CardDetail}>
+          <MaterialIcons name="account-circle" size={40} color="#212121" />
+          <View style={style.CardTexts}>
+            <Text
+              style={style.CardTitle}
+              ellipsizeMode="tail"
+              numberOfLines={2}>
+              {props.title}
+            </Text>
+            <Text>{props.channelTitle}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- src/components/MiniCard.tsx
+++ src/components/MiniCard.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Touchable,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const MiniCard = props => {
+  const navigation = useNavigation();
   return (
-    <View style={style.MiniCard}>
-      <Image
-        source={{
-          uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
-        }}
-        style={{width: '45%', height: 100}}
-      />
-      <View style={style.MiniCardTexts}>
-        <Text
-          style={style.MiniCardTitle}
-          ellipsizeMode="tail"
-          numberOfLines={3}>
-          {props.title}
-        </Text>
-        <Text style={{fontSize: 17}}>{props.channelTitle}</Text>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('videoplayer', {
+          videoId: props.videoId,
+          title: props.title,
+        })
+      }>
+      <View style={style.MiniCard}>
+        <Image
+          source={{
+            uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
+          }}
+          style={{width: '45%', height: 100}}
+        />
+        <View style={style.MiniCardTexts}>
+          <Text
+            style={style.MiniCardTitle}
+            ellipsizeMode="tail"
+            numberOfLines={3}>
+            {props.title}
+          </Text>
+          <Text style={{fontSize: 17}}>{props.channelTitle}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
