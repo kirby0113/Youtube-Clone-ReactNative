e220661909eb892a000c239bc439339a506506c4
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-07 21:46:01 +0900

    feat: settings画面にダークモード用のSwitchを追加

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Switch} from 'react-native';
 import Header from '../components/Header';
 
+import {useSelector, useDispatch} from 'react-redux';
+import {ThemeState} from '../types/State';
+
 const Settings = () => {
+  const isDarkMode = useSelector((state: ThemeState) => {
+    return state.isDarkMode;
+  });
+  const dispatch = useDispatch();
+
+  const changeTheme = () => {
+    dispatch({type: 'change_theme', payload: !isDarkMode});
+  };
   const style = StyleSheet.create({
     Settings: {
       flex: 1,
     },
+    SettingsView: {},
+    SettingUnit: {},
   });
   return (
     <View style={style.Settings}>
       <Header />
-      <Text>Subscribe</Text>
+      <View style={style.SettingsView}>
+        <View style={style.SettingUnit}>
+          <Text>ダークモード</Text>
+          <Switch onValueChange={changeTheme} value={isDarkMode} />
+        </View>
+      </View>
     </View>
   );
 };
